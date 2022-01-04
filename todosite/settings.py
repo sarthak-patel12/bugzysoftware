639850a391d66b11d38667ad2c38d974011e0e14
sarthak-patel12 <68072075+sarthak-patel12@users.noreply.github.com>
2022-01-04 11:51:56 +0530

    Update settings.py

--- todosite/settings.py
+++ todosite/settings.py
@@ -141,4 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
