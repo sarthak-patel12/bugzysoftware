0c3fb88dfd4478587a5329b9f19760594e1387c8
sarthak-patel12 <68072075+sarthak-patel12@users.noreply.github.com>
2022-01-04 10:45:32 +0530

    Add files via upload

---
+++ todosite/urls.py
@@ -0,0 +1,28 @@
+"""todosite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from todo.views import index,create_todo,todo_details
+from authentication.views import register,login
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',index, name ='home'),
+    path('login',login, name ='login'),
+    path('register',register, name ='register'),
+    path('create_todo',create_todo, name ='create_todo'),
+    path('todo/<user_id>',todo_details, name ='todo_details'),
+]
