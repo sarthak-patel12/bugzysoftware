96a0b9c4606cfcd2755467ba557a53f1e6fde048
sarthak-patel12 <68072075+sarthak-patel12@users.noreply.github.com>
2022-01-05 14:40:31 +0530

    Add files via upload

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from validate_email import validate_email
+from .models import User
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
 
 # Create your views here.
 def register(request):
+	if request.method == "POST":
+		context={'has_error': False,'data':request.POST}
+		email = request.POST.get('email')
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		password2 = request.POST.get('password2')
+
+		if len(password)<6 and password:
+			messages.add_message(request,messages.ERROR,'Password should be at least 6 characters')
+			context['has_error'] = True
+
+		if password!=password2:
+			messages.add_message(request,messages.ERROR,'Password mismatch')
+			context['has_error'] = True
+
+		if not validate_email(email) and email:
+			messages.add_message(request,messages.ERROR,'Invalid Email ! please enter a valid email')
+			context['has_error'] = True
+
+		if not username:
+			messages.add_message(request,messages.ERROR,'Username required')
+			context['has_error'] = True
+
+		if not email:
+			messages.add_message(request,messages.ERROR,'Email required')
+			context['has_error'] = True
+
+		if not password:
+			messages.add_message(request,messages.ERROR,'Password required')
+			context['has_error'] = True
+
+		if not password2:
+			messages.add_message(request,messages.ERROR,'Repeat Password required')
+			context['has_error'] = True
+
+		if User.objects.filter(username=username).exists() and email:
+			messages.add_message(request,messages.ERROR,'Username already exists! Please select another username')
+			context['has_error'] = True
+
+		if User.objects.filter(email=email).exists() and username:
+			messages.add_message(request,messages.ERROR,'Email already exists! Please select another Email')
+			context['has_error'] = True
+
+		if context['has_error']:
+			return render(request,'authentication/register.html',context)
+		user = User.objects.create_user(username=username,email=email)
+		user.set_password(password)
+		user.save()
+		messages.add_message(request,messages.SUCCESS,'Account Created Successfully, You can now Login')
+		return redirect('login')
 	return render(request,'authentication/register.html')
 
-def login(request):
+
+def login_user(request):
+	if request.method == "POST":
+		context={'has_error': False,'data':request.POST}
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request,username=username,password=password)
+		if not user:
+			messages.add_message(request,messages.ERROR,"Invalid Credentials")
+			return render(request,'authentication/login.html',context)
+		login(request,user)
+		messages.add_message(request,messages.SUCCESS,f'Welcome {user.username}')
+		return redirect(reverse('home'))
 	return render(request,'authentication/login.html')
+
+
+def logout_user(request):
+	logout(request)
+	messages.add_message(request,messages.SUCCESS,f'Successfully logged out')
+	return redirect(reverse('login'))
