eb2633687bf9783778ddcf2c770069abc121bba0
sarthak-patel12 <68072075+sarthak-patel12@users.noreply.github.com>
2022-01-05 22:02:03 +0530

    Update settings.py

--- todosite/settings.py
+++ todosite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6jln_=j$_$366cmtak4z!=-8hr-0%w04dzeahng*&p3_z=gciv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
